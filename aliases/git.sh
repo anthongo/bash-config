eb083aa86dcbb9933dfcafffe9e16b67d159f9cc
Anthony Ngo <anthonyngo002@gmail.com>
2025-10-11 19:00:29 -0700

    remove autopairs

---
+++ aliases/git.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+alias clone="gh repo clone"
+alias pull="git pull"
+alias push="git push"
+alias status="git status"
+alias commit="git commit --message"
--- plugins/autopairs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-# shellcheck disable=SC1090
-export BASH_AUTOPAIR_BACKSPACE=1
-source ~/.config/bash/bash-autopairs/autopairs.sh
